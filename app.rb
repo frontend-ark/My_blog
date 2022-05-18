1efc4b3a3e7244fab60f3c7d802e6b626a24260b
Yana Arkhipova <frontend.ark@gmail.com>
2022-05-18 22:23:43 +0600

    Initialize

---
+++ app.rb
@@ -0,0 +1,11 @@
+
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: 'sqlite3', database: 'Blog.db'} 
+
+get '/' do
+	erb :index
+end
