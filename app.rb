ecace02469106e2dccfa5084a42232dd54a821a3
Yana Arkhipova <frontend.ark@gmail.com>
2022-05-18 22:44:36 +0600

    Add Rakefile

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ require 'sinatra/activerecord'
 
 set :database, {adapter: 'sqlite3', database: 'Blog.db'} 
 
+class Post < ActiveRecord::Base
+end
+
+class Comment < ActiveRecord::Base
+end
+
 get '/' do
 	erb :index
 end
